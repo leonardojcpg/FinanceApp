44a8fded2fb0baef16a5648abd8409c773983ad9
LeonardoGomes <leonardojpinheirogomes@gmail.com>
2025-09-14 22:27:18 -0300

    fix: Alterando a classe service para corrigir problema de gravacao de usuario

--- src/main/java/finance/financeApp/service/UsuarioService.java
+++ src/main/java/finance/financeApp/service/UsuarioService.java
@@ -1,30 +1,40 @@
 package finance.financeApp.service;
 
-import finance.financeApp.model.*;
-import finance.financeApp.repository.*;
+import finance.financeApp.model.Usuario;
+import finance.financeApp.repository.UsuarioRepository;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Service
-public class UsuarioService {
+public class UsuarioService
+{
     private final UsuarioRepository repo;
 
-    public UsuarioService(UsuarioRepository repo) {
+    public UsuarioService(UsuarioRepository repo)
+    {
         this.repo = repo;
     }
 
-    public List<Usuario> findAll() {
+    public List<Usuario> findAll()
+    {
         return repo.findAll();
     }
 
-    public Optional<Usuario> findById(UUID id) {
+    public Optional<Usuario> findById(UUID id)
+    {
         return repo.findById(id);
     }
 
     public Usuario save(Usuario usuario) {
+        if (usuario.getSenha() == null || usuario.getSenha().isBlank()) {
+            throw new IllegalArgumentException("Senha não pode ser nula ou vazia");
+        }
+        if (usuario.getEmail() == null || usuario.getEmail().isBlank()) {
+            throw new IllegalArgumentException("Email não pode ser nulo ou vazio");
+        }
+
         return repo.save(usuario);
     }
 
