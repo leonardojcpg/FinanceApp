f4f6e0e4d3da676ed1f8a76fe598a7ba96749530
leonardogomes <leonardojpinheirogomes@gmail.com>
2025-09-16 16:56:08 -0300

    feat: Adicionando o secret no JwtUtil

--- src/main/java/finance/financeApp/util/JwtUtil.java
+++ src/main/java/finance/financeApp/util/JwtUtil.java
@@ -3,6 +3,7 @@ package finance.financeApp.util;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
@@ -10,8 +11,11 @@ import java.util.Date;
 
 @Component
 public class JwtUtil {
-    private final String secret = "umaChaveSecretaMuitoGrandeMesmoComMaisDe32Caracteres";
-    private final long expiration = 1000 * 60 * 60; // 1 hora
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private long expiration;
 
     private Key getSigningKey() {
         return Keys.hmacShaKeyFor(secret.getBytes());
@@ -19,9 +23,9 @@ public class JwtUtil {
 
     public String generateToken(String email) {
         return Jwts.builder()
-                .subject(email)
-                .issuedAt(new Date())
-                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
                 .signWith(getSigningKey())
                 .compact();
     }
@@ -39,10 +43,10 @@ public class JwtUtil {
     }
 
     private Claims getClaims(String token) {
-        return Jwts.parser()
-                .verifyWith(getSigningKey())
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
                 .build()
-                .parseSignedClaims(token)
-                .getPayload();
+                .parseClaimsJws(token)
+                .getBody();
     }
 }
